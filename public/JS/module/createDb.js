255c78d53a4bd93f31f050af50b5924ef28b316a
dalramjwi <dalramjwi@gmail.com>
2024-06-21 11:36:26 +0900

    수현_Feat_createDb 함수 제작 완료

--- public/JS/module/createDb.js
+++ public/JS/module/createDb.js
@@ -1,3 +1,8 @@
 const database = require("sqlite3").verbose();
 //경로 설정은 지윤님 경로 그대로 사용했습니다.
 const db = new database.Database("./public/JS/module/db");
+const createDb = (tableName) => {
+  db.run(
+    `CREATE TABLE ${tableName} (product TEXT NOT NULL, price INTEGER NOT NULL, position INTEGER NOT NULL)`
+  );
+};
