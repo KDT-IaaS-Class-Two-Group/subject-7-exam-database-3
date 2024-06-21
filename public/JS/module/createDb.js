a4a16e663306dd824b93b797654154aadfc012e7
dalramjwi <dalramjwi@gmail.com>
2024-06-21 11:44:20 +0900

    수현_Feat_create 함수 테스트 중 오류 발생

--- public/JS/module/createDb.js
+++ public/JS/module/createDb.js
@@ -1,8 +1,9 @@
 const database = require("sqlite3").verbose();
 //경로 설정은 지윤님 경로 그대로 사용했습니다.
-const db = new database.Database("./public/JS/module/db");
+const db = new database.Database("/public/JS/module/test.db");
 const createDb = (tableName) => {
   db.run(
     `CREATE TABLE ${tableName} (product TEXT NOT NULL, price INTEGER NOT NULL, position INTEGER NOT NULL)`
   );
 };
+createDb("TEST");
