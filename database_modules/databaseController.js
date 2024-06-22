58ac9886f41cbd38b5fa23d6022f649588fee015
naviadev <qo2oq1020@gmail.com>
2024-06-22 14:37:26 +0900

    Feat _ databaseController.js : 함수 , 클래스 각각 두 종류로 나눠 같은 역할을 수행하는 모듈 구현
    현재 create 부분만 동적으로 생성하였습니다. 외부 js 파일인 tableScheme에 존재하는 배열들을 매개변수로 받아 테이블을 생성하는 방식으로 작동됩니다.

--- database_modules/databaseController.js
+++ database_modules/databaseController.js
@@ -1,39 +1,76 @@
-const Database = require('sqlite3');
+const sqlite3 = require('sqlite3');
 const table = require('./tableShcema.js');
 
-
 const databaseManager = (tableName, columns) => {
-  const db = new Database.Database('./product.db');
+  const db = new sqlite3.Database('./product.db');
 
-  let obj = {
+  return {
+    tableName,
+    columns,
 
-    tableName: tableName,
-    columns: columns,
-
-    createTable: () => {
+    createTable() {
       const queryString = this.columns.map(column => {
         if (column.type) {
           return `${column.name} ${column.type}`;
-        }
-        else {
-          return column.foregin;
+        } else if (column.foreign) {
+          return column.foreign;
         }
       }).join(',');
 
-      db.run(`CREATE TABLE IF NOT EXISTS ${tableName} (${queryString})`);
-
+      const query = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${queryString})`;
+      db.run(query, (err) => {
+        if (err) {
+          console.error(`Error creating table ${this.tableName}:`, err.message);
+        } else {
+          console.log(`Table ${this.tableName} created successfully.`);
+        }
+      });
     },
+  };
+};
+
+// Object.keys(table).forEach((ele) => {
+//   const dbManager = databaseManager(ele, table[ele]);
+//   dbManager.createTable();
+// });
+
+// ----------------------------------------------------------------------------------------------------
+// 
+/**
+ * ? 위 작성한 함수를 class 형태로 변환하기.
+ */
+class DatabaseManager {
+  constructor(tableName, columns) {
+    this.tableName = tableName;
+    this.columns = columns;
+    this.db = new sqlite3.Database('./product.db');
   }
 
+  createTable() {
+    // map 메서드 -> 원본 배열을 해치지않고 인덱스를 순회하며 로직을 수행한 뒤 값을 반환하다.
 
-  return obj;
+    const columnString = this.columns.map(ele => {
+      if (ele.type) {
+        return `${ele.name} ${ele.type}`;
+      } else if (ele.foreign) {
+        return ele.foreign;
+      }
+    }).join(',');
+
+    const queryString = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${columnString})`;
+    
+    this.db.run(queryString, (err)=>{
+      if(err){
+        console.error(`Error creating table ${this.tableName}:`, err.message);
+      }else{
+        console.log(`Table ${this.tableName} created successfully.`);
+      }
+    })
+    
+  }
 }
 
-Object.keys(table).forEach(ele => {
-  const dbManager = new databaseManager(ele, table[ele]);
+Object.keys(table).forEach((ele) => {
+  const dbManager = new DatabaseManager(ele, table[ele]);
   dbManager.createTable();
 });
-
-// for(let i = 0; i < 3; i++){
-
-// }
