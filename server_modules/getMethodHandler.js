5e6e3f21ecc43c2d5b5a3548935be8f98fa4633c
yun <sinjiyun290@gamil.com>
2024-06-27 14:36:43 +0900

    지윤_Refactor_getMethodHandler 모듈을 뺐다.

---
+++ server_modules/getMethodHandler.js
@@ -0,0 +1,37 @@
+/**
+ * ? getMethodHandler : get 요청을 처리하는 모듈
+ * @param {*} req : 사용자 요청
+ * @param {*} res : 응답객체
+ * 
+ * ! 현재 진행한 작업 
+ * switch case "/" 처리
+ * 
+ * ! 진행해야하는 작업
+ * "login", "~~"과 같은 작업들을 수행해야할 필요가 있다. -> switch 구문에 모듈을 추가하는 방식으로 진행
+ */
+
+ const getMethodHandler = (req, res) => {
+
+    const url = req.url;
+    const contentType = getContentType(url);
+    let fileName;
+  
+    switch (true) {
+      case url === "/":
+        sendFile(`./${htmlPath}vending.html`, contentType, res);
+        break;
+  
+      case url.includes("/CSS"):
+        fileName = url.split("/CSS/")[1];
+        sendFile(`./${cssPath+fileName}`, contentType, res);
+        break;
+  
+      case url.includes("/img/"):
+        fileName = url.split('/img/')[1];
+        sendFile(`./img/${fileName}`, contentType, res);
+        break
+  
+      default:
+        break;
+    }
+  };
