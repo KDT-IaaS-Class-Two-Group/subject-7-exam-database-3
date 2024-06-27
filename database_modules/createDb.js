48065307da395abbcd16c99f7370ac5e371de125
zoeznm <bomikim046@gmail.com>
2024-06-27 15:47:57 +0900

    수현_Feat_createLoginDb func 만듬

--- database_modules/createDb.js
+++ database_modules/createDb.js
@@ -1,18 +1,19 @@
+const createLoginDb = (tableName)=>{
 const database = require("sqlite3").verbose();
-//경로 설정은 지윤님 경로 그대로 사용했습니다.
-const db = new database.Database("./public/JS/module/test.db", (err) => {
-  console.log("에러 발생 : ", err);
-});
-const createDb = (tableName) => {
-  db.run(
-    `CREATE TABLE ${tableName} (product TEXT NOT NULL, price INTEGER NOT NULL, position INTEGER NOT NULL)`,
-    (err) => {
-      if (err) {
-        console.log("오류 : ", err);
-      } else {
-        console.log("실행됨");
+  const db = new database.Database("./database/login.db", (err) => {
+    console.log("에러 발생 : ", err);
+  });
+  const createDb = (tableName) => {
+    db.run(
+      `CREATE TABLE ${tableName} (id TEXT NOT NULL, cookie INTEGER NOT NULL)`,
+      (err) => {
+        if (err) {
+          console.log("오류 : ", err);
+        } else {
+          console.log(`${tableName} 생성됨`);
+        }
       }
-    }
-  );
-};
-createDb("TEST");
+    );
+  };
+}
+
