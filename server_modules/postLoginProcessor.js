7b776f74b6f7ff4dddf9ac909605d982ca92e34f
dalramjwi <dalramjwi@gmail.com>
2024-06-27 16:38:48 +0900

    수현_Feat_vending.html sun에 event 추가하기 위해 연결함

--- server_modules/postLoginProcessor.js
+++ server_modules/postLoginProcessor.js
@@ -2,25 +2,22 @@
  * ? postLoginProcessor : req객체에 포함되어있는 id,pw값을 추출하여 db에서 확인하는 모듈
  * @param {*} req : 요청객체
  * @param {*} res : 응답객체
- * 
- * ! 현재 진행한 작업 
- * id, pw값 추출 
- * 
+ *
+ * ! 현재 진행한 작업
+ * id, pw값 추출
+ *
  * ! 진행해야하는 작업
  * Database 모듈 접근, response 반환
  */
 const postLoginProcessor = (req, res) => {
-    let body = "";
-    req.on("data", (data) => {
-        body += data;
-    });
-    req.on("end", () => {
-        const data = JSON.stringify(body);
-        const id = data.split("&")[0].split("id=")[1];
-        const pw = data.split("&")[1].split("password=")[1];
-        
-      // console.log(decodeURI(id));
-      // console.log(decodeURI(pw));
-    })
+  let body = "";
+  req.on("data", (data) => {
+    body += data;
+  });
+  req.on("end", () => {
+    const data = JSON.stringify(body);
+    const id = data.split("&")[0].split("id=")[1];
+    console.log(decodeURI(id));
+  });
 };
 module.exports = postLoginProcessor;
--- server_modules/postMethodHandler.js
+++ server_modules/postMethodHandler.js
@@ -1,9 +1,9 @@
-const postLoginProcessor = require("./postLoginProcessor")
+const postLoginProcessor = require("./postLoginProcessor");
 const postMethodHandler = (req, res) => {
-    switch (req.url) {
-        case "/signup":
-        postLoginProcessor(req, res);
-        break;
-    }
-}
+  switch (req.url) {
+    case "/login":
+      postLoginProcessor(req, res);
+      break;
+  }
+};
 module.exports = postMethodHandler;
