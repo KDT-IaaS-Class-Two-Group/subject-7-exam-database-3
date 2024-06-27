3defe266d00b22ed0d79a034da61a41a1b9cffd1
yun <sinjiyun290@gamil.com>
2024-06-27 14:51:26 +0900

    지윤_Refactor_sendFile을 모듈로 뺐다.

---
+++ server_modules/sendFile.js
@@ -0,0 +1,19 @@
+/**
+ * ? sendFile: 핸들러에서 받아온 fileName과 contentType을 기반으로 response 객체 전송
+ * @param {*} fileName 
+ * @param {*} contentType 
+ * @param {*} res 
+ */
+const sendFile = (fileName, contentType, res) => {
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 ERROR");
+        return;
+    }
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+    })
+};
+module.exports=sendFile;
