759f4ff0a8469710ba6f2728eeb26c60731de8dc
naviadev <qo2oq1020@gmail.com>
2024-06-27 17:33:09 +0900

    Refactor _ star.js : addEventListener 내부 로직을 모듈로 분리
    1. setKeyFrameDuration -> 키프레임 지정(애니메이션 길이)
    2. setSize -> 생성되 노드의 크기를 설정, 10~20px
    3. createNode -> 매개변수로 입력 받은 데이터를 기반으로 노드 생성
    4. randPosition -> 랜덤 위치에 포지션값을 지정

--- public/modules/star.js
+++ public/modules/star.js
@@ -1,21 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const starContainer = document.getElementById('body');
   const starsCount = 20;
+
   function createStar() {
-    const star = document.createElement('div');
-    star.classList.add('star');
-    const size = Math.random() * 10 + 10; // 10~20 px로 출력 되게끔;
-    star.style.width = `${size}px`;
-    star.style.height = `${size}px`;
-    // window 내부 랜덤한 위치에서 생성
-    star.style.top = `${Math.random() * window.innerHeight}px`;
-    star.style.left = `${Math.random() * window.innerWidth}px`;
-    starContainer.appendChild(star);
+    const star = createNode('div', 'star');
+    setSize(star);
+    randPosition(star);
+    document.body.appendChild(star);
+
     star.addEventListener('animationend', () => {
       star.remove();
     });
-    const blinkDuration = Math.random() * 2 + 1;  // 1~3초
-    star.style.animationDuration = `${blinkDuration}s`;
+
+    setKeyFrameDuration(star);
   }
 
   async function generateStars() {
@@ -23,8 +19,31 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(generateStars, Math.random() * 1000 + 500);
   }
 
-
   for (let i = 0; i < starsCount; i++) {
     generateStars();
   }
 });
+
+const setKeyFrameDuration = (htmlElement) => {
+  const keyFrameDuration = Math.random() * 2 + 1;
+  htmlElement.style.animationDuration = `${keyFrameDuration}s`;
+}
+
+const createNode = (tagName, ...className) => {
+  const node = document.createElement(tagName);
+  if (className !== null && className !== undefined) {
+    node.classList.add(className);
+  }
+  return node;
+}
+
+const setSize = (htmlElement) => {
+  const size = Math.random() * 10 + 10; // 10~20 px로 출력 되게끔;
+  htmlElement.style.width = `${size}px`;
+  htmlElement.style.height = `${size}px`;
+}
+
+const randPosition = (htmlElement) => {
+  htmlElement.style.top = `${Math.random() * window.innerHeight}px`;
+  htmlElement.style.left = `${Math.random() * window.innerWidth}px`;
+}
