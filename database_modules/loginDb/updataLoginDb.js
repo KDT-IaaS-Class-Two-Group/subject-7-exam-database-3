91fba358eac73f569408a749c851b42d6b6d9296
dalramjwi <dalramjwi@gmail.com>
2024-06-28 15:41:29 +0900

    지윤_Feat_login DB 행 중 state를 바꿔주기 위해 updataLoginDb를 작성했음

--- database_modules/loginDb/updataLoginDb.js
+++ database_modules/loginDb/updataLoginDb.js
@@ -1,8 +1,5 @@
 const database = require("sqlite3").verbose();
-const path = require("path");
-const db = new database.Database(
-  path.join(__dirname, "../../database/login.db")
-);
+const db = new database.Database("./database/login.db");
 const updateLoginDb = (tableName, change, changeValue, select, selectValue) => {
   const update = db.prepare(
     `UPDATE ${tableName} SET ${change} = ? WHERE ${select} = ?`
