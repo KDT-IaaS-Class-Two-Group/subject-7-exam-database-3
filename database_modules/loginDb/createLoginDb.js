41c8b2e90f904fbdcb624bd026e30f5e8e953857
dalramjwi <dalramjwi@gmail.com>
2024-07-01 12:54:41 +0900

    수현_Refactor_Promise 객체 사용

--- database_modules/loginDb/createLoginDb.js
+++ database_modules/loginDb/createLoginDb.js
@@ -1,14 +1,17 @@
 const createLoginDb = (db, tableName) => {
-  db.run(
-    `CREATE TABLE ${tableName} (id TEXT NOT NULL, cookie INTEGER, state TEXT)`,
-    (err) => {
-      if (err) {
-        console.log("오류 : ", err);
-      } else {
-        console.log(`${tableName} 생성됨`);
+  return new Promise((resolve, reject) => {
+    db.run(
+      `CREATE TABLE ${tableName} (id TEXT NOT NULL, cookie INTEGER, state TEXT)`,
+      (err) => {
+        if (err) {
+          reject("오류 : ", err);
+        } else {
+          console.log(`${tableName} 생성됨`);
+          resolve();
+        }
       }
-    }
-  );
+    );
+  });
 };
 
 module.exports = createLoginDb;
