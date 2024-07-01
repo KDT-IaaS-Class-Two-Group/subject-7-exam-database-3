543ed6353f1c8ab070372bfed14f14ee573121ef
dalramjwi <dalramjwi@gmail.com>
2024-07-01 12:47:08 +0900

    수현_Refactor_loginDb 파일 위치 변경

---
+++ database_modules/loginDB.js
@@ -0,0 +1,4 @@
+const createLoginDb = require("./loginDb/createLoginDb");
+const insertBasicLoginDb = require("./loginDb/insertBasicLoginDb");
+// createLoginDb("login");
+// insertBasicLoginDb("login");
