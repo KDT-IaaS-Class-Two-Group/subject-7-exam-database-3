1985858b6f5c61a70ee1f1547ccfd938eaaaf009
dalramjwi <dalramjwi@gmail.com>
2024-07-01 12:50:24 +0900

    수현_Refactor_db 선언 위치 옮김 및 함수 매개변수로 추가

--- database_modules/loginDB.js
+++ database_modules/loginDB.js
@@ -1,3 +1,7 @@
+const database = require("sqlite3").verbose();
+const db = new database.Database("./database/login.db", (err) => {
+  console.log("에러 발생 : ", err);
+});
 const createLoginDb = require("./loginDb/createLoginDb");
 const insertBasicLoginDb = require("./loginDb/insertLoginDb");
 // createLoginDb("login");
