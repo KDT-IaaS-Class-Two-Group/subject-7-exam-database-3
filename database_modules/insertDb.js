0f0c0c4aca41287f4769b0a19b0f8b93acf4dfc0
dalramjwi <dalramjwi@gmail.com>
2024-07-01 16:50:04 +0900

    지윤_Refactor_insert 수정

--- database_modules/insertDb.js
+++ database_modules/insertDb.js
@@ -1,20 +1,24 @@
 const products = require("./product");
+
 const insertDb = (db, tableName) => {
   return new Promise((resolve, reject) => {
-    for (let i = 0; i < products.length; i++) {
+    for (let i = 0; i < 5; i++) {
       const product = products[tableName][i][0];
-      const price = product[tableName][i][1];
-      const insertQuery = `INSERT INTO ${tableName} (product, price) VALUES (?, ?)`;
+      const price = products[tableName][i][1];
+      console.log(product, price);
+      const insertQuery = `INSERT INTO ${tableName} (productName, price) VALUES (?, ?)`;
 
-      db.run(insertQuery, [product], [price], (err) => {
+      db.run(insertQuery, [product, price], (err) => {
         if (err) {
           reject(err); // 에러 발생 시 reject 호출
+          console.log("sd");
+
           return;
         }
         console.log(`데이터 삽입됨: ${product}와 ${price}`);
+        resolve(); // 모든 삽입이 완료되면 resolve 호출
       });
     }
-    resolve(); // 모든 삽입이 완료되면 resolve 호출
   });
 };
 
