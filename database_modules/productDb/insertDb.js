7883d90eb866de511fc2d374170c6f7e48b3df78
dalramjwi <dalramjwi@gmail.com>
2024-07-01 17:12:23 +0900

    지윤_Refactor_insertDb에 설명추가

--- database_modules/productDb/insertDb.js
+++ database_modules/productDb/insertDb.js
@@ -4,10 +4,11 @@ const insertDb = (db, tableName) => {
   return new Promise((resolve, reject) => {
     for (let i = 0; i < 5; i++) {
       const product = products[tableName][i][0];
-      const price = products[tableName][i][1];
-      const insertQuery = `INSERT INTO ${tableName} (productName, price) VALUES (?, ?)`;
+      const description = products[tableName][i][1];
+      const price = products[tableName][i][2];
+      const insertQuery = `INSERT INTO ${tableName} (productName,description, price) VALUES (?, ?, ?)`;
 
-      db.run(insertQuery, [product, price], (err) => {
+      db.run(insertQuery, [product, description, price], (err) => {
         if (err) {
           reject(err); // 에러 발생 시 reject 호출
           return;
