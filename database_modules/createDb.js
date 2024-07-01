a84ca30039d5c62806f7986f4eb8a6400ce44d1d
dalramjwi <dalramjwi@gmail.com>
2024-07-01 15:57:34 +0900

    지윤_Feat_제품 데이터베이스 create 생성

---
+++ database_modules/createDb.js
@@ -0,0 +1,17 @@
+const createDb = (db, tableName) => {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `CREATE TABLE ${tableName} (productName TEXT NOT NULL, price INTEGER)`,
+      (err) => {
+        if (err) {
+          reject("오류 : ", err);
+        } else {
+          console.log(`${tableName} 생성됨`);
+          resolve();
+        }
+      }
+    );
+  });
+};
+
+module.exports = createDb;
