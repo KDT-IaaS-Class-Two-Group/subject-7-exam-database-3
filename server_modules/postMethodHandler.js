38b72a9e390f8db05692d00de5dc9faa4c1a6251
dalramjwi <dalramjwi@gmail.com>
2024-07-02 12:59:16 +0900

    지윤_Refactor_postMethodHandler.js에서 insert-coin부분 모듈화 처리 완료

--- server_modules/postMethodHandler.js
+++ server_modules/postMethodHandler.js
@@ -1,7 +1,6 @@
 const postLoginProcessor = require("./postLoginProcessor");
 const Gologin = require("./postGologin");
-let coinCount = 0; // 총 코인 카운트 변수
-const coinTypeCounts = {}; // 코인 종류별 카운트를 저장할 객체
+const insertCoin = require("./postInsert-coin");
 
 const postMethodHandler = (req, res) => {
   console.log("Handling POST request for:", req.url);
@@ -15,37 +14,7 @@ const postMethodHandler = (req, res) => {
         Gologin(req, res);
         break;
       case "/insert-coin": // insert-coin 엔드포인트 추가
-        let coinBody = "";
-        req.on("data", (data) => {
-          coinBody += data;
-        });
-        req.on("end", () => {
-          try {
-            const coinData = JSON.parse(coinBody);
-            console.log("Received coin data:", coinData);
-
-            // 코인 종류별 카운트 증가
-            const coinType = coinData.data.split("-")[1];
-            if (!coinTypeCounts[coinType]) {
-              coinTypeCounts[coinType] = 0;
-            }
-            coinTypeCounts[coinType]++;
-            coinCount++; // 총 코인 카운트 증가
-
-            // 코인 종류별 누적 카운트 및 총 카운트 출력
-            console.log(`Total coins inserted: ${coinCount}`);
-            Object.keys(coinTypeCounts).forEach((type) => {
-              console.log(`${type} coins inserted: ${coinTypeCounts[type]}`);
-            });
-
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ coinCount, coinTypeCounts }));
-          } catch (error) {
-            console.error("Error parsing JSON:", error);
-            res.writeHead(400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Invalid JSON" }));
-          }
-        });
+        insertCoin(req, res); // insert-coin 요청 처리
         break;
       default:
         res.writeHead(404, { "Content-Type": "text/plain" });
