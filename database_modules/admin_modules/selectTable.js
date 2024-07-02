56c93a5ec5691c06dab4d24269de26545d7104a5
naviadev <qo2oq1020@gmail.com>
2024-07-02 15:58:49 +0900

    Fix _ selectTable.js : 정적 선언 추가, sessionTable 메서드 추가

--- database_modules/admin_modules/selectTable.js
+++ database_modules/admin_modules/selectTable.js
@@ -1,13 +1,13 @@
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 
 class SelectUserData {
   constructor(databaseName) {
     this.db = new sqlite3.Database(`./${databaseName}.db`);
   }
 
-  select(tableName, obj) {
+  selectAdminTable(obj) {
     const { name, pw } = obj;
-    const query = `SELECT * FROM ${tableName} WHERE name = ? AND pw = ?`;
+    const query = `SELECT * FROM admin WHERE name = ? AND pw = ?`;
 
     return new Promise((resolve, reject) => {
       this.db.get(query, [name, pw], (error, row) => {
@@ -18,7 +18,22 @@ class SelectUserData {
         resolve(row);
       });
     });
+  };
+
+  selectSessionTable(obj) {
+    const { sessionKey } = obj;
+    const query = `SELECT * FROM session WHERE sessionKey = ?`;
+    return new Promise((resolve, reject) => {
+      this.db.get(query, sessionKey, (error, row) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(row);
+      })
+    })
   }
+
+
 }
 
 module.exports = SelectUserData;
