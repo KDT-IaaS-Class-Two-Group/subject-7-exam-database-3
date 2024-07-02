8b2790bdef8cd82256e49964d5053e237ecb24b9
naviadev <qo2oq1020@gmail.com>
2024-07-02 15:54:42 +0900

    Fix _ keyDownEvnet.js : 모든 유효성 검사가 완료되어야 fetch를 보낼 수 있도록 수정하였다.

--- public/modules/event/keyDownEvent.js
+++ public/modules/event/keyDownEvent.js
@@ -2,36 +2,44 @@ export class EnterEvent {
   constructor() {
     this.event();
   }
-
   event() {
     window.addEventListener('keydown', async (e) => {
       if (e.key === "Enter") {
-        const loginData = {
-          id: document.getElementById('id').value,
-          pw: document.getElementById('pw').value
-        }
-        try {
-          const response = await fetch('http://localhost:3000/adminLogin', {
-            method: "POST",
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(loginData)
-          });
 
-          if (!response.ok) {
-            throw new Error('실패');
+        const id = document.getElementById('id');
+        const pw = document.getElementById('pw');
+
+        if (id.style.borderColor === 'green' && pw.style.borderColor === 'green') {
+          const loginData = {
+            name: document.getElementById('id').value,
+            pw: document.getElementById('pw').value
           }
+          try {
+            const response = await fetch('http://localhost:3000/adminLogin', {
+              method: "POST",
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(loginData)
+            });
 
-          const data = await response.json();
-          console.log('야호', data);
+            if (!response.ok) {
+              throw new Error('실패');
+            }
 
-          // 성공 시 페이지 이동 등 추가 로직
-          // window.location.href = '/dashboard'; // 예: 대시보드 페이지로 이동
+            const data = await response.json();
 
-        } catch (error) {
-          console.error('Error:', error);
+            // 성공 시 페이지 이동 등 추가 로직
+            // window.location.href = '/dashboard'; // 예: 대시보드 페이지로 이동
+
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        } else {
+          return;
         }
+
+
       }
     });
   }
