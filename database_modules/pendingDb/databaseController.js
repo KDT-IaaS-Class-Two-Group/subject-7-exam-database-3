82cd8d797e0e64c1e8129d7e4349013aec36cd5b
dalramjwi <dalramjwi@gmail.com>
2024-07-02 12:05:03 +0900

    수현_Refactor_폴더 생성 및 사용하지 않는 모듈 정리

--- database_modules/pendingDb/databaseController.js
+++ database_modules/pendingDb/databaseController.js
@@ -1,21 +1,23 @@
-const sqlite3 = require('sqlite3');
-const table = require('./tableScheme.js');
+const sqlite3 = require("sqlite3");
+const table = require("../tableScheme.js");
 
 const databaseManager = (tableName, columns) => {
-  const db = new sqlite3.Database('./product.db');
+  const db = new sqlite3.Database("./product.db");
 
   return {
     tableName,
     columns,
 
     createTable() {
-      const queryString = this.columns.map(column => {
-        if (column.type) {
-          return `${column.name} ${column.type}`;
-        } else if (column.foreign) {
-          return column.foreign;
-        }
-      }).join(',');
+      const queryString = this.columns
+        .map((column) => {
+          if (column.type) {
+            return `${column.name} ${column.type}`;
+          } else if (column.foreign) {
+            return column.foreign;
+          }
+        })
+        .join(",");
 
       const query = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${queryString})`;
       db.run(query, (err) => {
@@ -35,7 +37,7 @@ const databaseManager = (tableName, columns) => {
 // });
 
 // ----------------------------------------------------------------------------------------------------
-// 
+//
 /**
  * ? 위 작성한 함수를 class 형태로 변환하기.
  */
@@ -43,30 +45,31 @@ class DatabaseManager {
   constructor(tableName, columns) {
     this.tableName = tableName;
     this.columns = columns;
-    this.db = new sqlite3.Database('./product.db');
+    this.db = new sqlite3.Database("./product.db");
   }
 
   createTable() {
     // map 메서드 -> 원본 배열을 해치지않고 인덱스를 순회하며 로직을 수행한 뒤 값을 반환하다.
 
-    const columnString = this.columns.map(ele => {
-      if (ele.type) {
-        return `${ele.name} ${ele.type}`;
-      } else if (ele.foreign) {
-        return ele.foreign;
-      }
-    }).join(',');
+    const columnString = this.columns
+      .map((ele) => {
+        if (ele.type) {
+          return `${ele.name} ${ele.type}`;
+        } else if (ele.foreign) {
+          return ele.foreign;
+        }
+      })
+      .join(",");
 
     const queryString = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${columnString})`;
-    
-    this.db.run(queryString, (err)=>{
-      if(err){
+
+    this.db.run(queryString, (err) => {
+      if (err) {
         console.error(`Error creating table ${this.tableName}:`, err.message);
-      }else{
+      } else {
         console.log(`Table ${this.tableName} created successfully.`);
       }
-    })
-    
+    });
   }
 }
 
