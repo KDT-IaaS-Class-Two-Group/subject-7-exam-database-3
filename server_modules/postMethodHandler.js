62ee38c70d9e538740c9a7c0e86c345133d26229
dalramjwi <dalramjwi@gmail.com>
2024-07-02 12:53:51 +0900

    지윤_Refactor_postMethodHandler.js안의 Gologin부분 모듈 진행 완료

--- server_modules/postMethodHandler.js
+++ server_modules/postMethodHandler.js
@@ -1,6 +1,5 @@
-const path = require("path");
-const sendFile = require("./sendFile");
 const postLoginProcessor = require("./postLoginProcessor");
+const Gologin = require("./postGologin");
 let coinCount = 0; // 총 코인 카운트 변수
 const coinTypeCounts = {}; // 코인 종류별 카운트를 저장할 객체
 
@@ -13,29 +12,7 @@ const postMethodHandler = (req, res) => {
         postLoginProcessor(req, res);
         break;
       case "/Gologin":
-        let body = "";
-        req.on("data", (data) => {
-          body += data;
-        });
-        req.on("end", () => {
-          try {
-            const data = JSON.parse(body);
-            console.log(data);
-            const filePath = path.join(
-              __dirname,
-              "..",
-              "public",
-              "HTML",
-              "index.html"
-            );
-            const contentType = "text/html";
-            sendFile(filePath, contentType, res);
-          } catch (error) {
-            console.error("Error parsing JSON:", error);
-            res.writeHead(400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Invalid JSON" }));
-          }
-        });
+        Gologin(req, res);
         break;
       case "/insert-coin": // insert-coin 엔드포인트 추가
         let coinBody = "";
@@ -48,7 +25,7 @@ const postMethodHandler = (req, res) => {
             console.log("Received coin data:", coinData);
 
             // 코인 종류별 카운트 증가
-            const coinType = coinData.data.split('-')[1];
+            const coinType = coinData.data.split("-")[1];
             if (!coinTypeCounts[coinType]) {
               coinTypeCounts[coinType] = 0;
             }
@@ -57,7 +34,7 @@ const postMethodHandler = (req, res) => {
 
             // 코인 종류별 누적 카운트 및 총 카운트 출력
             console.log(`Total coins inserted: ${coinCount}`);
-            Object.keys(coinTypeCounts).forEach(type => {
+            Object.keys(coinTypeCounts).forEach((type) => {
               console.log(`${type} coins inserted: ${coinTypeCounts[type]}`);
             });
 
