fe12f57c228f9cb5ff891755d9feaf9167161558
naviadev <qo2oq1020@gmail.com>
2024-07-09 09:42:34 +0900

    Fix | getLoginData.js : node -> id 변수로 변경

--- public/modules/auth/getLoginData.js
+++ public/modules/auth/getLoginData.js
@@ -1,7 +1,7 @@
 export const getLoginData = () => {
   const id = document.getElementById('id');
   const pw = document.getElementById('pw');
-  if (idNode.style.borderColor === 'green' && pwNode.style.borderColor === 'green') {
+  if (id.style.borderColor === 'green' && pw.style.borderColor === 'green') {
     return {
       name: id.value,
       pw: pw.value
