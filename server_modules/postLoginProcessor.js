6033c5d66cd63b7ae421b251336c64f926a713a4
naviadev <qo2oq1020@gmail.com>
2024-07-10 19:55:40 +0900

    Refator _ post 라우터 공통 로직 단일화 postloginProcessor_Admin.js postLoginProcessor.js postMethodHandler.js
    공통으로 사용되는 req.on을 하나의 모듈로 처리

--- server_modules/postLoginProcessor.js
+++ server_modules/postLoginProcessor.js
@@ -2,25 +2,13 @@ const selectDb = require("../database_modules/loginDb/selectLoginDb");
 const postLoginSelect = require("./postLoginSelect");
 const sendFile = require("./sendFile"); // sendFile 모듈 로드
 
-const postLoginProcessor = (req, res) => {
-  let body = "";
-  req.on("data", (data) => {
-    body += data;
+const postLoginProcessor = (data, res) => {
+  const id = data.id;
+  console.log("Received ID:", id);
+  selectDb("*", "login", "id", id, (err, rows) => {
+    postLoginSelect(res, id, err, rows);
   });
-  req.on("end", () => {
-    try {
-      const data = JSON.parse(body);
-      const id = data.id;
-      console.log("Received ID:", id);
-      selectDb("*", "login", "id", id, (err, rows) => {
-        postLoginSelect(res, id, err, rows);
-      });
-    } catch (error) {
-      console.error("Error parsing JSON:", error);
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "Invalid JSON" }));
-    }
-  });
+
 };
 
 module.exports = postLoginProcessor;
