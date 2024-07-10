ca8837430eb82b4fcc3075b843f4473f3a2503bf
naviadev <qo2oq1020@gmail.com>
2024-07-10 19:55:40 +0900

    Refator _ post 라우터 공통 로직 단일화 postloginProcessor_Admin.js postLoginProcessor.js postMethodHandler.js
    공통으로 사용되는 req.on을 하나의 모듈로 처리

--- server_modules/postMethodHandler.js
+++ server_modules/postMethodHandler.js
@@ -2,33 +2,39 @@ const path = require("path");
 const sendFile = require("./sendFile");
 const adminLoginProcessor = require('./postLoginProcessor_Admin');
 const postLoginProcessor = require("./postLoginProcessor");
+const searchUser = require("./searchUser");
 
 const postMethodHandler = (req, res) => {
-  switch (req.url) {
-    case "/login":
-      postLoginProcessor(req, res);
-      break;
-    case "/Gologin":
-      let body = "";
-      req.on("data", (data) => {
-        body += data;
-      });
-      req.on("end", () => {
-        const data = JSON.parse(body);
-        console.log(data);
+  let body = "";
+  req.on("data", (data) => {
+    body += data;
+  });
+  req.on("end", async () => {
+    const data = await JSON.parse(body);
+    switch (req.url) {
+      case "/login":
+        postLoginProcessor(data, res);
+        break;
+
+      case "/Gologin":
         const filePath = path.join(__dirname, "..", "public", "HTML", "index.html");
         const contentType = "text/html";
         sendFile(filePath, contentType, res);
-      });
-      break;
-    case "/adminLogin":
-      adminLoginProcessor(req, res);
-      break;
-    default:
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Not Found");
-      break;
-  }
-};
+        break;
 
+      case "/adminLogin":
+        adminLoginProcessor(data, res);
+        break;
+
+      case "/search":
+        searchUser(data, res);
+        break;
+
+      default:
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+        break;
+    }
+  });
+}
 module.exports = postMethodHandler;
