261a66dfeda77f3ee1fbd35568c1d05f549cf952
naviadev <qo2oq1020@gmail.com>
2024-07-10 20:26:08 +0900

    fix _ module : 모듈 전역 할당 주석처리

--- server_modules/checkAuth.js
+++ server_modules/checkAuth.js
@@ -2,6 +2,8 @@ const AdminManager = require('../database_modules/admin_modules/adminManager.js'
 const CreateTable = require('../database_modules/admin_modules/createTable.js')
 const SelectTable = require('../database_modules/admin_modules/selectTable.js');
 
+// checkAuth();
+
 const checkAuth = async (obj) => {
   const adminManager = new AdminManager(new CreateTable('admin'), new SelectTable('admin'));
   const result = await adminManager.checkData(obj)
