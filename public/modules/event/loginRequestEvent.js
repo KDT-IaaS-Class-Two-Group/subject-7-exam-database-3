53080d8daa8f818b92b44fd59ee7ef8221c56a70
naviadev <qo2oq1020@gmail.com>
2024-07-10 14:56:59 +0900

    Refactor _ loginRequestEvent.js : 기존 EnterEvent class에 할당되어있던 기능을 모듈로 분리

---
+++ public/modules/event/loginRequestEvent.js
@@ -0,0 +1,31 @@
+import { getLoginData } from "../auth/getLoginData.js";
+import { ViewChangeManager } from "../componentManager/viewChangeManager.js";
+import { searchUserEvent } from "./templateViewInputEvent.js";
+import { templateView } from "../component/productTemplate.js";
+
+
+const loginRequestEvent = async (e) => {
+  if (e.key === "Enter") {
+    const loginData = getLoginData();
+    if (loginData) {
+      try {
+        const response = await fetch('http://localhost:3000/adminLogin', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(loginData)
+        });
+        if (!response.ok) {
+          throw new Error('실패');
+        } else {
+          const changeView = new ViewChangeManager('main-container');
+          changeView.changeView(templateView());
+          searchUserEvent();
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  }
+};
